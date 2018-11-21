533a9afc7458fd9391d53f2af9002996a088542a
wailimku <37812463+wailimku@users.noreply.github.com>
2018-11-21 16:04:53 -0500

    Add files via upload

--- src/figure1_code/peaks_overlap_for_3T3_cells.r
+++ src/figure1_code/peaks_overlap_for_3T3_cells.r
@@ -1,3 +1,6 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
 library(utils)
 library(chromVAR)
 library(motifmatchr)
@@ -12,21 +15,21 @@ register(MulticoreParam(16, progressbar = TRUE))
 ############################
 # kkk=1 for Fig 1d, kkk=2 for Extended Data Fig 1c, kkk=3 for Extended Data Fig 1d
 ############################
-kkk<-2 #Plese change
+kkk<-args[1] #Plese change
 ############################
 peaks <- getPeaks('./data/output/Figure1/KZ730-GA6909_0_0_mapq10_noDup-W200-G200-E.00000001.scoreisland',sort_peaks = TRUE)
 
 peaks <-unique(peaks)
 peaks<-sort(peaks)
 
-chicpeaks <- getPeaks('./data/output/Figure1/KZ1252_GB0640_0_0_mapq10_noDup-W200-G200-E500.scoreisland',sort_peaks = TRUE)
+chicpeaks <- getPeaks('./data/temp/Figure1/GSM3475877_KZ1252_GB0640_0_0_mapq10_noDup-W200-G200-E500.scoreisland',sort_peaks = TRUE)
 
 
 
-chicpeaks3000 <- getPeaks('./data/output/Figure1/KZ1225_GB0451_0_0_mapq10_noDup-W200-G200-E100.scoreisland',sort_peaks = TRUE)
-chicpeaks1000 <- getPeaks('./data/output/Figure1/KZ1245-2_0_0_mapq10_noDup-W200-G200.scoreisland',sort_peaks = TRUE)
-chicpeaks300 <- getPeaks('./data/output/Figure1/KZ1245-3_0_0_mapq10_noDup-W200-G200.scoreisland',sort_peaks = TRUE)
-chicpeaks100 <- getPeaks('./data/output/Figure1/KZ1245-4_0_0_mapq10_noDup-W200-G200.scoreisland',sort_peaks = TRUE)
+chicpeaks3000 <- getPeaks('./data/temp/Figure1/GSM3475847_GB0451_0_0_mapq10_noDup-W200-G200-E100.scoreisland',sort_peaks = TRUE)
+chicpeaks1000 <- getPeaks('./data/temp/Figure1/GSM3475849_GB0475_0_0_mapq10_noDup-W200-G200.scoreisland',sort_peaks = TRUE)
+chicpeaks300 <- getPeaks('./data/temp/Figure1/GSM3475851_GB0476_0_0_mapq10_noDup-W200-G200.scoreisland',sort_peaks = TRUE)
+chicpeaks100 <- getPeaks('./data/temp/Figure1/GSM3475853_GB0477_0_0_mapq10_noDup-W200-G200.scoreisland',sort_peaks = TRUE)
 
 
 
@@ -69,7 +72,7 @@ dda<-c(xd3000[1]/pd3000[1],xd1000[1]/pd1000[1],xd300[1]/pd300[1],xd100[1]/pd100[
 
 if(kkk==2)
 {
-	pdf("./data/Figures/Figure1/barplot_precision_3T3_low_cell_num.pdf")
+	pdf("./Figures/Figure1/barplot_precision_3T3_low_cell_num.pdf")
 	barplot(dda, main="scChIC-seq for 3T3 ",ylab="Precison",xlab="Number of cells", names.arg=c("3000", "1000 ", "300","100 "), cex.names=1.5,,cex.main=1.5, cex.lab=1.5,cex.axis=1.5)
 	dev.off() 
 }
@@ -79,10 +82,11 @@ pda<-c(xd3000[1],xd1000[1],xd300[1],xd100[1])
 
 if(kkk==3)
 {
-	pdf("./data/Figures/Figure1/barplot_senstivity_3T3_low_cell_num.pdf")
+	pdf("./Figures/Figure1/barplot_senstivity_3T3_low_cell_num.pdf")
 	barplot(pda, main="scChIC-seq for 3T3 ",ylab="Number of Peaks",xlab="Number of cells", names.arg=c("3000", "1000 ", "300","100 "), cex.names=1.5,,cex.main=1.5, cex.lab=1.5,cex.axis=1.5)
 	dev.off() 
 }
+
 
 
 
